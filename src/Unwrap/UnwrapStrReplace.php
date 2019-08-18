b7a41a51135e05ed9fc13de1209b4b06bbb731f4
sasezaki <sasezaki@gmail.com>
2019-08-18 21:59:15 +0900

    Adding UnwrapStrReplace

---
+++ src/Unwrap/UnwrapStrReplace.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sfp\Infection\Mutator\Unwrap;
+
+use Infection\Mutator\Unwrap\AbstractUnwrapMutator;
+use PhpParser\Node;
+
+final class UnwrapStrReplace extends AbstractUnwrapMutator
+{
+    protected function getFunctionName(): string
+    {
+        return 'str_replace';
+    }
+
+    protected function getParameterIndexes(Node $node): \Generator
+    {
+        yield 2;
+    }
+}
